5fd16d549d73e31fe11d2fe1900bfa68ceaeed27
nishantlad-18 <ladnishant07@gmail.com>
2026-08-25 22:07:04 +0530

    first string program

---
+++ String/program1.java
@@ -0,0 +1,12 @@
+package CoreJavaPreparation.String;
+
+class program1 {
+    public static void main(String[] args){
+
+        String str = "Nishant";
+        String str2 = new String("Nishant");
+
+        System.out.println(str);
+        System.out.println(str2);
+    }    
+}

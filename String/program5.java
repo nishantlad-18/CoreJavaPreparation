c640bb8a87dc85794a443378d97b3a2f4abccc03
nishantlad-18 <ladnishant07@gmail.com>
2026-08-26 23:43:12 +0530

    string concat using operator

---
+++ String/program5.java
@@ -0,0 +1,11 @@
+package CoreJavaPreparation.String;
+
+class program5 {
+    public static void main(String[] args){
+        String n = "Nishant";
+        String n1 = " Lad";
+
+        String m = n + n1;
+        System.out.println(m);
+    }    
+}

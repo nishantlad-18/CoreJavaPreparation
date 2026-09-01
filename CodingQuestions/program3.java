9818a260bf522d4da6d2e616c3de51b9647e4c9a
nishantlad-18 <ladnishant07@gmail.com>
2026-09-01 23:54:40 +0530

    Factors pattern

---
+++ CodingQuestions/program3.java
@@ -0,0 +1,16 @@
+package CoreJavaPreparation.CodingQuestions;
+import java.util.*;
+
+class program3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= num; i++){
+            if(num % i == 0){
+                System.out.println(i + " is a factor of "+num);
+            }
+        }
+    }
+}
